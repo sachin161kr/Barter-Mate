69b6885c5e9809b773931e0733b8511e46cb555e
Sachin Kumar <mail.sachin161@gmail.com>
2022-04-12 09:28:04 +0530

    Support UI updated

--- screens/Support.js
+++ screens/Support.js
@@ -1,5 +1,8 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, Image} from 'react-native';
+
+import {scale, verticalScale, moderateScale} from 'react-native-size-matters';
+import query from '../assets/query.png';
 
 const SupportScreen = () => {
   return (
@@ -7,9 +10,26 @@ const SupportScreen = () => {
       <View
         style={{
           flex: 1,
-          backgroundColor: '#FFFFFF',
+          backgroundColor: '#EF6563',
         }}>
-        <Text>Support</Text>
+        <View
+          style={{
+            backgroundColor: '#EF6563',
+            height: verticalScale(400),
+            marginHorizontal: scale(20),
+            borderRadius: moderateScale(10),
+            marginTop: verticalScale(60),
+          }}>
+          <Image
+            source={query}
+            style={{
+              height: verticalScale(250),
+              resizeMode: 'contain',
+              alignSelf: 'center',
+              marginTop: verticalScale(50),
+            }}
+          />
+        </View>
       </View>
     </>
   );
